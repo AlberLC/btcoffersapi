874fbdd8c3a9c67a69ab714bcda90dcab19bd717
AlberLC <alberlc@outlook.com>
2025-04-16 20:53:10 +0200

    Fix database_lock creation when already exists

--- btcoffersapi/database/locks.py
+++ btcoffersapi/database/locks.py
@@ -17,14 +17,17 @@ async def database_lock(lock: bool = True) -> AsyncIterator[None]:
         ):
             await asyncio.sleep(1)
 
-        await lock_collection.insert_one(
+        await lock_collection.update_one(
+            {'_id': 'offers_lock'},
             {
-                '_id': 'offers_lock',
-                'until': datetime.datetime.now(datetime.timezone.utc) + config.database_lock_expiration
-            }
+                '$set': {
+                    'until': datetime.datetime.now(datetime.timezone.utc) + config.database_lock_expiration
+                }
+            },
+            upsert=True
         )
 
     yield
 
     if lock:
-        await lock_collection.delete_many({'_id': 'offers_lock'})
+        await lock_collection.delete_one({'_id': 'offers_lock'})
