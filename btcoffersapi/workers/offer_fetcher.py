f7a04a78f4a80eb042ce43374c8ba7ee8b286736
AlberLC <alberlc@outlook.com>
2025-04-18 15:14:25 +0200

    Fix Yadio api call
    (cherry picked from commit b4650ae68089d6f6f1cb2d6b711bf9c95ce7c8c5)

--- btcoffersapi/workers/offer_fetcher.py
+++ btcoffersapi/workers/offer_fetcher.py
@@ -3,18 +3,19 @@ import asyncio
 import aiohttp
 
 from btcoffersapi.config import config
+from btcoffersapi.database.locks import database_lock
 from btcoffersapi.database.repositories.offer_repository import OfferRepository
 from btcoffersapi.services import hodlhodl, lnp2pbot, robosats
-from btcoffersapi.database.locks import database_lock
 
 
 async def fetch_offers() -> None:
     offer_repository = OfferRepository()
 
     while True:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(config.yadio_api_endpoint) as response:
-                yadio_data = await response.json()
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(config.yadio_api_endpoint) as response:
+                    yadio_data = await response.json()
 
             offers = (
                 *await hodlhodl.fetch_offers(session, yadio_data['EUR']['USD'], yadio_data['BTC']),
@@ -24,6 +25,15 @@ async def fetch_offers() -> None:
             async with database_lock():
                 await offer_repository.delete_many({})
                 await offer_repository.insert_many(offers)
+                offers = (
+                    *await hodlhodl.fetch_offers(session, yadio_data['EUR']['USD'], yadio_data['BTC']),
+                    *await robosats.fetch_offers(session, yadio_data['EUR']['USD']),
+                )
+                async with database_lock():
+                    await offer_repository.delete_many({})
+                    await offer_repository.insert_many(offers)
+        except aiohttp.ClientConnectorError:
+            pass
 
         await asyncio.sleep(config.fetch_offers_every.total_seconds())
 
