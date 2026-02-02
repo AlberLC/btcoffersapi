ead447554dad27ba3c0987615f521e17b480fd74
AlberLC <alberlc@outlook.com>
2026-02-02 11:43:16 +0100

    Add payment methods and exchanges to offer notifier

--- btcoffersapi/services/offer_notifier.py
+++ btcoffersapi/services/offer_notifier.py
@@ -4,6 +4,7 @@ import json
 import fastapi.encoders
 from fastapi import WebSocket, WebSocketDisconnect, websockets
 
+from api.schemas.enums import Exchange, PaymentMethod
 from config import config
 from database.repositories.offer_repository import OfferRepository
 
@@ -12,8 +13,18 @@ async def notify_offers(
     websocket: WebSocket,
     offer_repository: OfferRepository,
     chat_id: int,
-    query: dict[str, float]
+    raw_query: dict[str, float | list[str]]
 ) -> None:
+    query = {}
+
+    for k, v in raw_query.items():
+        if k == 'payment_methods':
+            v = [PaymentMethod(payment_method) for payment_method in raw_query['payment_methods']]
+        elif k == 'exchanges':
+            v = [Exchange(exchange) for exchange in raw_query['exchanges']]
+
+        query[k] = v
+
     while not (offers_data := await offer_repository.get(**query)):
         await asyncio.sleep(config.offers_fetch_sleep)
 
