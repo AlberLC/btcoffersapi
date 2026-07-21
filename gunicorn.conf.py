3c10cbf6a04697134cbdbfc604bfcad8306ad27a
AlberLC <alberlc@outlook.com>
2026-07-21 20:56:08 +0200

    Update gunicorn.conf

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,6 +3,7 @@ import multiprocessing
 import gunicorn.arbiter
 import uvicorn_worker
 
+from config import config as config_
 from workers import offer_fetcher
 
 
@@ -10,7 +11,7 @@ def on_starting(_arbiter: gunicorn.arbiter.Arbiter) -> None:
     multiprocessing.Process(target=offer_fetcher.run, daemon=True).start()
 
 
-bind = '0.0.0.0:5211'
+bind = f'{config_.api_host}:{config_.api_port}'
 preload_app = True
 worker_class = uvicorn_worker.UvicornWorker
 workers = 4
