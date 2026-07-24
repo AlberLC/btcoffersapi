fea994d2546f859249a01e4aae916c90e820cb56
AlberLC <alberlc@outlook.com>
2026-07-24 04:06:41 +0200

    Remove useless code

--- btcoffersapi/database/client.py
+++ btcoffersapi/database/client.py
@@ -1,18 +1,7 @@
-from bson import ObjectId
-from bson.errors import InvalidId
-from fastapi import HTTPException, status
 from pymongo import AsyncMongoClient
 from pymongo.asynchronous.database import AsyncDatabase
 
 from config import config
-
-
-def create_object_id(id: str) -> ObjectId:
-    try:
-        return ObjectId(id)
-    except InvalidId:
-        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=f'{id} is not a valid ObjectId')
-
 
 client = AsyncMongoClient(username=config.mongo_username, password=config.mongo_password, tz_aware=True)
 database: AsyncDatabase = client[config.database_name]
